641bd667840dcf66088e1bf0e7863acb5fc91587
gracy jayant <gracyj29@gmail.com>
2020-08-05 13:48:26 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Header from './Header.js';
 
 
 
@@ -9,6 +10,7 @@ class App extends Component {
          <div className="header">
            Phone Directory
          </div>
+         <Header/>
          <button> Add </button>
          <div>
            <span>Name</span> <br />
